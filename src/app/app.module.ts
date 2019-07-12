003c9394286439dbff2c33ac74f5ff3e9313eaf0
niiccolas <40167702+niiccolas@users.noreply.github.com>
2019-07-12 11:32:46 +0200

    Main app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
